03d1cfbe4c43a1568e581e275e4b8def7a241334
Hoanqnqu <nhathoangthuychaub1@gmail.com>
2023-04-26 04:06:28 +0700

    updateaccount

--- src/services/user.js
+++ src/services/user.js
@@ -16,3 +16,17 @@ export const getOne = (id)=> new Promise(async(resolve, reject)=>{
         reject(error)
     }
 })
+export const updateUser = (payload, id)=> new Promise(async(resolve, reject)=>{
+    try {
+        const response = await db.User.update(payload,{
+            where:{id},
+        })
+        resolve({
+            err: response[0] > 0 ? 0:1,
+            msg: response[0] > 0? 'Ok': 'Failed to get updateUser',
+            response
+        })
+    } catch (error) {
+        reject(error)
+    }
+})
